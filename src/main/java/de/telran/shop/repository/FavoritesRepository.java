edec418e9e7cb843b92791f0220976ceb4054371
Yulia Vladimirov <julia.vladimirov@gmail.com>
2024-05-22 22:22:49 +0200

    Repositories added, DBs created (Radomyr Chaiuk, Yulia Vladimirov)

--- src/main/java/de/telran/shop/repository/FavoritesRepository.java
+++ src/main/java/de/telran/shop/repository/FavoritesRepository.java
@@ -4,5 +4,4 @@ import de.telran.shop.entity.Favorites;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoritesRepository extends JpaRepository<Favorites,Long> {
-
 }
