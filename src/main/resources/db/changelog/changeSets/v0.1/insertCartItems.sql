d1bf9eb82d23cf005c678a34e0591f903cc43495
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-05-23 14:29:55 +0300

    add liquibase

---
+++ src/main/resources/db/changelog/changeSets/v0.1/insertCartItems.sql
@@ -0,0 +1,8 @@
+-- liquibase formatted sql
+
+-- changeset alex2808:insert_cartitems
+insert into cartitems (CartId,Quantity,ProductId)
+values (1,5,1),
+       (2,8,2),
+       (3,10,3),
+       (1,22,2)
