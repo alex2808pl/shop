b207a2c62ed3d43f51416124d0f843ff918b1f22
4rad95 <4rad@i.ua>
2024-06-05 12:43:23 +0200

    New services filesCreate using Mappers.java.

--- src/main/java/de/telran/shop/mapper/Mappers.java
+++ src/main/java/de/telran/shop/mapper/Mappers.java
@@ -1,5 +1,11 @@
 package de.telran.shop.mapper;
 
+import de.telran.shop.dto.CartDto;
+import de.telran.shop.dto.CartItemsDto;
+import de.telran.shop.dto.FavoritesDto;
+import de.telran.shop.entity.Cart;
+import de.telran.shop.entity.CartItems;
+import de.telran.shop.entity.Favorites;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -9,6 +15,34 @@ import org.springframework.stereotype.Component;
 public class Mappers {
 
     private final ModelMapper modelMapper;
+
+        public CartDto convertToCartDto(Cart cart) {
+        return modelMapper.map(cart,CartDto.class);
+    }
+
+    public Cart convertToCart(CartDto cartDto) {
+        return modelMapper.map(cartDto, Cart.class);
+    }
+
+    public CartItemsDto convertToCartItemsDto(CartItems cartItems) {
+        return modelMapper.map(cartItems,CartItemsDto.class);
+    }
+
+    public CartItems convertToCartItems(CartItemsDto cartItemsDto) {
+        return modelMapper.map(cartItemsDto, CartItems.class);
+    }
+
+    public FavoritesDto convertToFavoritesDto(Favorites favorites) {
+        return modelMapper.map(favorites,FavoritesDto.class);
+    }
+
+    public Favorites convertToFavorites(FavoritesDto favoritesDto) {
+        return modelMapper.map(favoritesDto, Favorites.class);
+    }
+
+
+
+
 
 //    public RecipientDto convertToRecipientDto(Recipient recipient) {
 //        // перенастраиваю автомат, чтобы он пропустил поле iban и не отдал его клиенту
