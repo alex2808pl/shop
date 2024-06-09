7e35d26e128558d8dbae39b5802d6ccbda57bddf
Yulia Vladimirov <julia.vladimirov@gmail.com>
2024-06-09 19:16:02 +0200

    Service- and ControllerTests created. Final

--- src/test/java/de/telran/shop/controller/OrderItemsControllerTest.java
+++ src/test/java/de/telran/shop/controller/OrderItemsControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
-
 import java.math.BigDecimal;
 import java.util.List;
 
